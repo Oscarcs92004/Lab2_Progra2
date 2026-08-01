aaffcb7530082512cd8a4e8934056fe05505fd7b
Oscarcs92004 <oscarcs92004@gmail.com>
2026-08-01 08:39:02 -0600

    agrego atributos a empleado y los metodos vacios para que podamos trabajar en la herencia

--- Laboratorio2/src/Empleado.java
+++ Laboratorio2/src/Empleado.java
@@ -3,11 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.util.Calendar;
 /**
  *
  * @author oscar
  */
 public class Empleado {
+    private String codigoUnico;
+    private String nombre;
+    private Calendar fechaContratacion;
+    private double salarioBase;
+    private int horasTrabajadas;
+    private String img;
     
+    public void registrarHorasTrabajadas(){
+    
+    }
+    
+    public double calcularPago(){
+        // borrar el return 0 despues ahorita esta solo para que no tire error
+        return 0;
+    }
+    
+    public void mostrarInformacion(){
+    
+    }
 }
