4113bbeb7a715d60ff05f7cc86bdb387919bc4b1
leandr0gh <leandrosandoval131205@gmail.com>
2026-08-01 10:18:12 -0600

    Agregar jframe a main

--- Laboratorio2/src/Main.java
+++ Laboratorio2/src/Main.java
@@ -1,4 +1,18 @@
+import java.awt.Color;
+import javax.swing.*;
 
-public class Main {
+public class Main extends JFrame{
     
+    
+    public Main(){
+        
+        
+        setSize(800,800);
+        setBackground(Color.gray);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+    }
 }
