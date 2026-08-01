a68df4c68467a88fd1beeff7d6ada55cb1e4c798
leandr0gh <leandrosandoval131205@gmail.com>
2026-08-01 09:29:02 -0600

    Agregar atributos de empleado temporal

--- Laboratorio2/src/EmpleadoTemporal.java
+++ Laboratorio2/src/EmpleadoTemporal.java
@@ -1,13 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author oscar
- */
-public class EmpleadoTemporal extends Empleado{
+import java.util.*;
+
+public class EmpleadoTemporal extends Empleado {
+
+    private Calendar fechaFinDeContrato;
+
+    public EmpleadoTemporal(String codigoUnico, String nombre, String img) {
+        super(codigoUnico, nombre, img);
+        this.fechaFinDeContrato = Calendar.getInstance();
+        fechaContratacion = Calendar.getInstance();
+        salarioBase = 15000;
+        horasTrabajadas = 0;
+
+    }
+    @Override
+    public double calcularPago() {
+        
+        
+        
+        return 0;
+    }
+
+    public void actualizarFechaFinContrato(Calendar nuevaFechaFinContrato) {
+
+    }
     
+    @Override
+    public String mostrarInformacion(){
+    super.mostrarInformacion();
+    
+    return "";}
 }
